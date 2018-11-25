398ce29199e8e50ea7d09a212bbdf31ffba5b282
Cam Webb <cw@camwebb.info>
2018-11-24 17:46:16 -0900

    man page

--- aregex.c
+++ aregex.c
@@ -1,11 +1,11 @@
 /*
  * aregex.c - Gawk extension to access the TRE approximate regex.
- * Copyright (C) 2018 Cam Webb
- * Distributed under the Gnu Pulbic Licence v3
+ * Copyright (C) 2018 Cam Webb, <cw@camwebb.info>
+ * Distributed under the GNU Pulbic Licence v3
  */
 
 #include "common.h"
-#include <tre/tre.h>
+#include <tre/tre.h>  // From the TRE regex lib. https://laurikari.net/tre/
 
 static awk_value_t * do_aregex(int nargs, awk_value_t *result API_FINFO_ARG)
 {
@@ -19,18 +19,19 @@ static awk_value_t * do_aregex(int nargs, awk_value_t *result API_FINFO_ARG)
       lintwarn(ext_id, _("aregex() called with >3 arguments"));
   }
 
-  // set the cost
+  // Set the default max cost
   int defcost = 2;
   if (get_argument(2, AWK_NUMBER, &incost)) defcost = incost.num_value ; 
 
-  // If the string arguments are read
+  // If the string arguments are read...
   if ((get_argument(0, AWK_STRING, &re)) &&     \
       (get_argument(1, AWK_STRING, &str))) {
     
     // Compile RE
     regex_t preg;
     tre_regcomp(&preg, re.str_value.str, REG_EXTENDED);
-    
+
+    // Set approx grep params
     regaparams_t params = { 0 };
     params.cost_ins   = 1;
     params.cost_del   = 1;
@@ -42,21 +43,26 @@ static awk_value_t * do_aregex(int nargs, awk_value_t *result API_FINFO_ARG)
     params.max_err    = defcost;
   
     regamatch_t match;
-    match.nmatch = 0;
-    match.pmatch = 0;
+    match.nmatch = 0; // No partial match arrays needed
+    match.pmatch = 0; //   - ditto -
     
     int treret = 0;
     int rval = 0;
-    
+
+    // Do the approx regexp
     treret = tre_regaexec(&preg, str.str_value.str, &match, params, 0);
 
     if (treret == REG_NOMATCH) rval = -1;
     else if (treret == REG_ESPACE) {
       rval = -999;
-      warning(ext_id, _("aregex: TRE error: temporary memory could not be allocated\n      to complete the matching operation."));
+      warning(ext_id, \
+        _("aregex: TRE err.: mem. insufficient to complete the match."));
     }
     else rval = match.cost;
 
+    // Return: cost (Levenshtein distance) if success, -1 if no match,
+    //   -999 if memory error
+    
     return make_number(rval, result);
   }
   
@@ -64,7 +70,7 @@ static awk_value_t * do_aregex(int nargs, awk_value_t *result API_FINFO_ARG)
   return make_null_string(result);
 }
 
-// Boilerplate:
+// Gawkextlib boilerplate:
 
 static awk_ext_func_t func_table[] = \
     {
@@ -81,3 +87,4 @@ static awk_bool_t (*init_func)(void) = init_my_module;
 static const char *ext_version = PACKAGE_STRING;
 
 dl_load_func(func_table, fmatch, "")
+
