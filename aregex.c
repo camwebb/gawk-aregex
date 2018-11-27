c979659629a5527232359c40b547fbf6ae0776d6
Cam Webb <cw@camwebb.info>
2018-11-27 08:50:38 -0900

    Major changes to API

--- aregex.c
+++ aregex.c
@@ -6,9 +6,13 @@
 
 // Minimal headers:
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/stat.h>
 #include <gawkapi.h>
 #include <tre/tre.h>
+
+#define NSUBMATCH 10
 
 // Gawkextlib boilerplate:
 static const gawk_api_t *api;	/* for convenience macros to work */
@@ -16,71 +20,121 @@ static awk_ext_id_t ext_id;
 int plugin_is_GPL_compatible;
 
 // Main aregex() function definition
-static awk_value_t * do_aregex(int nargs, awk_value_t *result, struct awk_ext_func *unused)
+static awk_value_t * do_aregex(int nargs, awk_value_t *result, \
+                            struct awk_ext_func *unused)
 {
   // Variables for reading awk function's arguments
   awk_value_t re;
   awk_value_t str;
-  awk_value_t incost;
-
-  // Check args if gawk called with lint 
-  if (do_lint) {
-    if (nargs > 3)
-      lintwarn(ext_id, "aregex() called with >3 arguments");
-  }
-
-  // Set the default max cost, and test for 3rd argument
-  int defcost = 2;
-  if (get_argument(2, AWK_NUMBER, &incost)) defcost = incost.num_value ; 
-
-  // If the string arguments (1st and 2nd) are read...
-  if ((get_argument(0, AWK_STRING, &re)) &&     \
-      (get_argument(1, AWK_STRING, &str))) {
-    
-    // Compile regex
-    regex_t preg;
-    tre_regcomp(&preg, re.str_value.str, REG_EXTENDED);
-
-    // Set approx aregex params
-    regaparams_t params = { 0 };
-    params.cost_ins   = 1;
-    params.cost_del   = 1;
-    params.cost_subst = 1;
-    params.max_cost   = defcost;
-    params.max_del    = defcost;
-    params.max_ins    = defcost;
-    params.max_subst  = defcost;
-    params.max_err    = defcost;
-
-    // Create structure for details of match
-    regamatch_t match;
-    match.nmatch = 0; // No partial match arrays needed
-    match.pmatch = 0; //   - ditto -
-
-    // Return values
-    int treret = 0;
-    int rval = 0;
-
-    // Do the approx regexp
-    treret = tre_regaexec(&preg, str.str_value.str, &match, params, 0);
-
-    // Set the do_aregex() return value depending on tre_regaexec() return:
-    // Return cost (Levenshtein distance) if success, -1 if no match,
-    //   -999 if memory error
-    if (treret == REG_NOMATCH) rval = -1;
-    else if (treret == REG_ESPACE) {
-      rval = -999;
-      warning(ext_id, \
-        "aregex: TRE err.: mem. insufficient to complete the match.");
-      // TO DO?: update_ERRNO_int(). But which ERRNO?
-    }
-    else rval = match.cost;
-    
-    return make_number(rval, result);
-  }
+  awk_value_t costs;
+  awk_value_t substr;
   
-  // On string argument failure:
-  return make_null_string(result);
+  // Read 3rd argument
+  if (!get_argument(2, AWK_ARRAY, &costs)) {
+    fatal(ext_id, "aregex: 3rd argument must be present, and an array");
+  }
+
+  // Read parameters for tre_regaexec()
+  awk_value_t val;
+  awk_value_t index;
+  int i;
+  char c[30];
+  const char *parami[8];
+  int paramv[8];
+  parami[0] = "cost_ins";   paramv[0] = 1;
+  parami[1] = "cost_del";   paramv[1] = 1;
+  parami[2] = "cost_subst"; paramv[2] = 1;
+  parami[3] = "max_cost";   paramv[3] = 3;
+  parami[4] = "max_del";    paramv[4] = 3;
+  parami[5] = "max_ins";    paramv[5] = 3;
+  parami[6] = "max_subst";  paramv[6] = 3;
+  parami[7] = "max_err";    paramv[7] = 3;
+  for (i = 0; i < 8; i++) {
+    make_const_string(parami[i], strlen(parami[i]), &index);
+    if (get_array_element(costs.array_cookie, &index, AWK_STRING, &val)) {
+      paramv[i] = atoi(val.str_value.str);
+      strcpy(c,"") ;
+      sprintf(c, "%s = %d", parami[i], atoi(val.str_value.str));
+      warning(ext_id, c);
+    }
+  }
+
+  // If the string arguments (1st and 2nd) are read. Q: die, or return NULL?
+  if (!get_argument(0, AWK_STRING, &re))
+    fatal(ext_id, "aregex: first parameter not found; must be a string");
+  if (!get_argument(1, AWK_STRING, &str))
+    fatal(ext_id, "aregex: second parameter not found; must be a string");
+  
+  // Compile regex
+  regex_t preg;
+  tre_regcomp(&preg, re.str_value.str, REG_EXTENDED);
+    
+  // Set approx aregex params
+  regaparams_t params = { 0 };
+  params.cost_ins   = paramv[0]; 
+  params.cost_del   = paramv[1];
+  params.cost_subst = paramv[2];
+  params.max_cost   = paramv[3];
+  params.max_del    = paramv[4];
+  params.max_ins    = paramv[5];
+  params.max_subst  = paramv[6];
+  params.max_err    = paramv[7];
+
+  // Create structure for details of match
+  regamatch_t match ;
+  match.nmatch = NSUBMATCH; // No partial match arrays needed
+  match.pmatch = (regmatch_t *) malloc(NSUBMATCH * sizeof(regmatch_t));
+  // match.pmatch; //   - ditto -
+  //regmatch_t match.pmatch[4];
+  
+  // Return values
+  int treret = 0;
+  int rval = 0;
+
+  // Do the approx regexp
+  treret = tre_regaexec(&preg, str.str_value.str, &match, params, 0);
+  char c2[20];
+  sprintf(c2, "offset: %d", match.pmatch[2].rm_so); 
+  warning(ext_id, c2);
+  
+  // Set the do_aregex() return value depending on tre_regaexec() return:
+  // Return cost (Levenshtein distance) if success, -1 if no match,
+  if (treret == REG_NOMATCH) rval = -1;
+  else rval = match.cost;
+  // Catch a "mem. not. allocated" return from tre_regaexec()
+  if (treret == REG_ESPACE) {
+    warning(ext_id,                                                     \
+            "aregex: TRE err., mem. insufficient to complete the match.");
+    return make_null_string(result);
+  }
+
+  
+  // Hand the substrings over to the substring array
+  // Read the SUBSEP symbol
+  awk_value_t subsep;
+  if (!sym_lookup("SUBSEP", AWK_STRING, &subsep))
+    warning(ext_id, "aregex: Could not get SUBSEP from gawk.");
+
+  // Read 4th argument
+  if (!get_argument(3, AWK_ARRAY, &substr)) {
+    warning(ext_id, "aregex: 4th argument not present or not an array");
+  }
+  else clear_array(substr.array_cookie);
+  
+  char outindex1[20];
+  char outval1[20];
+  awk_value_t outindex;
+  awk_value_t outval;
+  for (i = 1 ; i < match.nmatch; i++) {
+    if (match.pmatch[i].rm_so > 0) {
+      sprintf(outindex1, "%d", i);
+      sprintf(outval1, "%d %.*s", match.pmatch[i].rm_so+1, match.pmatch[i].rm_eo - match.pmatch[i].rm_so, str.str_value.str + match.pmatch[i].rm_so);
+      set_array_element(substr.array_cookie,                            \
+                        make_const_string(outindex1, strlen(outindex1), &outindex), \
+                        make_const_string(outval1, strlen(outval1), &outval));
+    }
+  }
+  return make_number(rval, result);
 }
 
 
